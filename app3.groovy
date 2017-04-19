bb939124464ad973557b6726032d92ee7049b24b
佐々木 伸也 <s-shinya@sasakinoMacBook-Pro.local>
2017-04-19 23:42:44 +0900

    add comment to app3.groovy

--- app3.groovy
+++ app3.groovy
@@ -1,10 +1,14 @@
+// @Grabはgroovyでライブラリを読み込むアノテーション
 @Grab("thymeleaf-spring4")
 @Controller
 class App {
+  // @ResponseBodyの場合、レスポンスとしてはModelAndViewクラスを返す
   @RequestMapping("/")
   @ResponseBody
   def home(ModelAndView mav) {
+    // viewの名前設定
     mav.setViewName("home")
+    // groovyではメソッドの最後の値が返り値になる
     mav
   }
 }
